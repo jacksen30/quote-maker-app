eb82ae5fe0e8c8636a28e8cdc2bb01e5393e3f90
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-01-17 19:06:57 +0800

    Header update

--- src/components/TitleBanner.js
+++ src/components/TitleBanner.js
@@ -2,19 +2,18 @@ import React from 'react'
 
 export default function TitleBanner() {
   return (
-    <>
+    <div className='titleBanner-wrapper'>
       <div className='titleWrapper' >
           <div className='nav-top'>
               <h1 className='qmkrLogo'>Quotemkr<button id='signUp-Button'>Sign Up</button></h1>
           </div>
+          </div>
+          
+      
+      <div className='nav-bottom'>
+          <h4 className='intro-text'>Quote-Mkr helps you to design branded quote content within 10 seconds !</h4>
       </div>
-      <div>
-        <p className='intro-text'>
-          <h4>Quote-Mkr helps you to design branded quote content within 10 seconds !</h4>
-          <h4>Perfect for quote lovers, content creators, businesses & theme pages</h4>
-        </p>
-      </div>
-    </>
+    </div>
   )
 }
 
