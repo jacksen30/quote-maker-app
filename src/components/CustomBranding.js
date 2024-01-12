aaa1c19dcb8c77714f340f8b5ad032222026dc57
Jacksen Nillson <nillsonjacksen@gmail.com>
2024-01-12 16:08:00 +0800

    test dropdown quote selector styling of width fix

--- src/components/CustomBranding.js
+++ src/components/CustomBranding.js
@@ -35,58 +35,6 @@ export default function CustomBranding(props) {
           </div>
         </fieldset>
 
-        {/* <fieldset className='branding-border-btn-div'>
-          <legend>Add Border to Branding:</legend>
-          <fieldset className='branding-border-thickness-btn-div'>
-            <legend>Border Thickness:</legend>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-thickness' id='brand-border-none' onClick={() => handleBrandBorderChange('width', '0px')}></input>
-              <label className='radio-btn-label' htmlFor='brand-border-none'>None</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-thickness' id='brand-border-thin' onClick={() => handleBrandBorderChange('width', '1px')} />
-              <label className='radio-btn-label' htmlFor='brand-border-thin'>Thin</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-thickness' id='brand-border-medium' onClick={() => handleBrandBorderChange('width', '2px')} />
-              <label className='radio-btn-label' htmlFor='brand-border-medium'>Medium</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-thickness' id='brand-border-thick' onClick={() => handleBrandBorderChange('width', '3px')}  />
-              <label className='radio-btn-label' htmlFor='brand-border-thick'>Thick</label>
-            </div>
-          </fieldset>
-          <fieldset className='branding-border-color-btn-div'>
-            <legend>Border Color:</legend>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-color' id='brand-border-color-white' onClick={() => handleBrandBorderChange('color', 'white')}></input>
-              <label className='radio-btn-label' htmlFor='brand-border-color-white'>White</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-color' id='brand-border-color-black' onClick={() => handleBrandBorderChange('color', 'black')} />
-              <label className='radio-btn-label' htmlFor='brand-border-color-black'>Black</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-color' id='brand-border-color-gray' onClick={() => handleBrandBorderChange('color', 'gray')} />
-              <label className='radio-btn-label' htmlFor='brand-border-color-gray'>Gray</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-color' id='brand-border-color-pink' onClick={() => handleBrandBorderChange('color', 'deeppink')}  />
-              <label className='radio-btn-label' htmlFor='brand-border-color-pink'>Pink</label>
-            </div>
-          </fieldset>
-          <fieldset className='branding-border-radius-btn-div'>
-            <legend>Border Radius:</legend>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-radius' id='brand-border-radius-square' onClick={() => handleBrandBorderChange('radius', '0px')}></input>
-              <label className='radio-btn-label' htmlFor='brand-border-radius-square'>Square</label>
-            </div>
-            <div>
-              <input className='radio-btn-input' type='radio' name='border-radius' id='brand-border-radius-rounded' onClick={() => handleBrandBorderChange('radius', '30px')} />
-              <label className='radio-btn-label' htmlFor='brand-border-radius-rounded'>Rounded</label>
-            </div>
-          </fieldset>
-        </fieldset> */}
       </ToggleSwitch>
     </div>
   )
